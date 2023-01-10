60236a41bb97a1b0d8d0e48a6780b90ab54a03f5
Juls Terobias <julayterobias@gmail.com>
2023-01-10 17:18:10 +0800

    Implement offer links to product and to add cart

--- templates/offer.php
+++ templates/offer.php
@@ -10,10 +10,10 @@ if( isset( $params['offers'] ) ):
         <li>
             <div>
             <?php if( !empty( $offers['image'] ) ): ?>
-                <a href=""><img src="<?php echo esc_attr( $offers['image'] ); ?>" title="<?php echo esc_attr( $offers['title'] ); ?>"></a>
-                <a href=""><span class="codecorun-por-title codecorun-por-attrs"> <?php echo esc_attr( $offers['title'] ); ?> </span></a>
+                <a href="<?php echo esc_attr( get_the_permalink( $offers['id'] ) ); ?>"><img src="<?php echo esc_attr( $offers['image'] ); ?>" title="<?php echo esc_attr( $offers['title'] ); ?>"></a>
+                <a href="<?php echo esc_attr( get_the_permalink( $offers['id'] ) ); ?>"><span class="codecorun-por-title codecorun-por-attrs"> <?php echo esc_attr( $offers['title'] ); ?> </span></a>
                 <span class="codecorun-por-price codecorun-por-attrs"> <?php echo $offers['price']; ?> </span>
-                <a href="" class="button">Add to cart</a>
+                <a href="<?php echo wc_get_cart_url(); ?>?add-to-cart=<?php echo esc_attr( $offers['id'] ); ?>" target="_blank" class="button">Add to cart</a>
             <?php endif; ?>
             </div>
         </li>
